d1d09c50299bee818621c81977a0bcc4fe32293f
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-15 15:00:52 -0700

    qwe

--- main.hpp
+++ main.hpp
@@ -1,3 +1,50 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int writeFile(string filename){
+    ofstream ofs;
+    int j,id;
+    string en,dn;
+    double sal;
+    cin>>j;
+    ofs.open(filename);
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+    ofs<<j<<"\n";
+for (size_t i = 0; i < j; i++)
+{
+    cin>>id>>en>>dn>>sal;
+    ofs<<id<<"\n"<<en<<"\n"<<dn<<"\n"<<sal<<"\n";
+}
+    ofs.close();
+    return j;
+}
+
+
+int readFile(string filename)
+{
+    ifstream ifs;
+    int j,id;
+    string en,dn;
+    double sal,total, avg;
+    ifs.open(filename);
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    ifs>>j;
+    for (size_t i = 0; i < j; i++)
+    {
+        ifs>>id>>en>>dn>>sal;
+        total=total+sal;
+        cout<<id<<" "<<en<<" "<<dn<<" "<<sal<<endl;
+        
+
+    }
+    return j;
+}
